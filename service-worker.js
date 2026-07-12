fbf0b6f52778fb22925b4defe5a435c5cc899c00
rete-seggi-fdi <coixmun@gmail.com>
2026-07-12 17:35:03 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
    Le richieste verso il backend (Apps Script, altro dominio) non vengono
    mai intercettate: passano sempre direttamente alla rete. */
 
-const VERSIONE = 'rete-seggi-v4';
+const VERSIONE = 'rete-seggi-v4.3';
 const CACHE_SHELL = 'shell-' + VERSIONE;
 const CACHE_DATI = 'dati-' + VERSIONE;
 
