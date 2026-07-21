5b7df4f3f16d4d82c5bf19e3d9cdf573e067a09f
rete-seggi-fdi <coixmun@gmail.com>
2026-07-21 18:02:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 /* Configurazione ambiente: non inserire segreti in questo file. */
 window.SEGGI_CONFIG = Object.freeze({
-  appVersion: '13.3.8',
+  appVersion: '13.3.9',
   buildDate: '2026-07-21',
-  environment: 'production',
+  environment: 'production', // 'test' oppure 'production'
   latestVersionUrl: 'build-info.json',
   appName: 'SeggioLink Roma',
-  backendProvider: 'apps-script',
-  backendUrl: 'https://script.google.com/macros/s/AKfycbxWFI-n2AjRW0uuEy95V00yW_HLmFzomK9Ya--izJCR/exec',
+  backendProvider: 'apps-script', // 'apps-script' oppure 'cloudflare-d1'
+  backendUrl: 'https://script.google.com/macros/s/AKfycbx9s5sLN-O6_BN5dZgU6oxjqkw5R9lV31BrC6S5T560dylxZdrBMWv2V9PXeoUkc1k5qw/exec',
   requestTimeoutMs: 20000,
   enabledMunicipalities: ['09'],
   allowAllMunicipalitiesData: false
