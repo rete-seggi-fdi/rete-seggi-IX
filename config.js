e27ef3b3fa76a50a007c23c303f155120dd6210e
rete-seggi-fdi <coixmun@gmail.com>
2026-07-22 10:22:17 +0200

    Update environment and backend configuration

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 window.SEGGI_CONFIG = Object.freeze({
   appVersion: '13.3.9',
   buildDate: '2026-07-21',
-  environment: 'production', // 'test' oppure 'production'
+  environment: 'production',
   latestVersionUrl: 'build-info.json',
   appName: 'SeggioLink Roma',
-  backendProvider: 'apps-script', // 'apps-script' oppure 'cloudflare-d1'
-  backendUrl: 'https://script.google.com/macros/s/AKfycbx9s5sLN-O6_BN5dZgU6oxjqkw5R9lV31BrC6S5T560dylxZdrBMWv2V9PXeoUkc1k5qw/exec',
+  backendProvider: 'apps-script',
+  backendUrl: 'https://script.google.com/macros/s/AKfycby6OzgGBRZ4IXIHm5wGhIsz0pWV7O4Y_XDat0sSgwV6rqeAdoqj14AsId712iU9wFj6tA/exec',
   requestTimeoutMs: 20000,
   enabledMunicipalities: ['09'],
   allowAllMunicipalitiesData: false
