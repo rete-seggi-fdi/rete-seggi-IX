1ca3fd1dc0659d1b5ea60d8f0b84eb1a8e563353
rete-seggi-fdi <coixmun@gmail.com>
2026-08-30 17:39:41 +0200

    Update service worker version to v13.7.6

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
    Le richieste verso il backend (Apps Script, altro dominio) non vengono
    mai intercettate: passano sempre direttamente alla rete. */
 
-const VERSIONE = 'seggiolink-v13.7.4-security';
+const VERSIONE = 'seggiolink-v13.7.6-security';
 const CACHE_SHELL = 'shell-' + VERSIONE;
 const CACHE_DATI = 'dati-' + VERSIONE;
 
